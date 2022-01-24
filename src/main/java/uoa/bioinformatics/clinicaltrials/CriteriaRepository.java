cba54f2f6db11a282044953a37fc7540c7c24393
Flaner6 <christosmpalotis@gmail.com>
2022-01-24 21:40:11 +0200

    added Sponsor entity + fix

--- src/main/java/uoa/bioinformatics/clinicaltrials/CriteriaRepository.java
+++ src/main/java/uoa/bioinformatics/clinicaltrials/CriteriaRepository.java
@@ -3,6 +3,6 @@ package uoa.bioinformatics.clinicaltrials;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-public interface CriteriaRepository extends CrudRepository<Trial, String> {
+public interface CriteriaRepository extends CrudRepository<Criteria, String> {
     List<Criteria> findCriteriaById(String str);
 }
